38dbe9cc1b2441f209e1f1576be3f27e57955df8
Marcello C <marcello_ct@hotmail.com>
2025-10-19 22:17:19 -0300

    Adding scroll event listeners and fetch species from API

--- js/script.js
+++ js/script.js
@@ -3,13 +3,26 @@ let searchPokemon = 1;
 
 // Pokemon class: Represents an individual Pokémon
 class Pokemon {
-  constructor(name, id, height, weight, types, sprite) {
+  constructor(
+    name,
+    id,
+    height,
+    weight,
+    types,
+    sprite,
+    habitat,
+    generation,
+    text
+  ) {
     this.name = name;
     this.id = id;
     this.height = height;
     this.weight = weight;
     this.types = types;
     this.sprite = sprite;
+    this.habitat = habitat;
+    this.generation = generation;
+    this.text = text;
   }
 
   // Method to display Pokémon info
@@ -23,6 +36,9 @@ class Pokemon {
     document.getElementById("pokemon-height").innerText = this.height;
     document.getElementById("pokemon-weight").innerText = this.weight;
     document.getElementById("pokemon-types").innerText = this.types.join(", ");
+    document.getElementById("pokemon-habitat").innerText = this.habitat;
+    document.getElementById("pokemon-generation").innerText = this.generation;
+    document.getElementById("pokemon-text").innerText = this.text;
 
     document.getElementById("search-field").value = "";
     searchPokemon = this.id;
@@ -43,6 +59,11 @@ class PokemonAPI {
           typeof nameOrId === "String" ? nameOrId.toLowerCase() : nameOrId
         }`
       );
+      const species = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${
+          typeof nameOrId === "String" ? nameOrId.toLowerCase() : nameOrId
+        }`
+      );
 
       if (!response.ok) {
         document.getElementById("error-message").innerText =
@@ -53,8 +74,12 @@ class PokemonAPI {
       }
 
       const data = await response.json();
+      const specie = await species.json();
 
       console.log(data);
+      console.log(specie);
+
+      console.log(specie["flavor_text_entries"][0]);
       // Build a Pokemon object
       return new Pokemon(
         data.name,
@@ -64,7 +89,10 @@ class PokemonAPI {
         data.types.map((t) => t.type.name),
         data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
           "front_default"
-        ]
+        ],
+        specie.habitat.name,
+        specie.generation.name,
+        specie["flavor_text_entries"][0]["flavor_text"].replace(/[\r\n]/g, " ")
       );
     } catch (error) {
       console.error(error);
@@ -110,4 +138,21 @@ document.querySelector(".btn-next").addEventListener("click", async () => {
   const api = new PokemonAPI();
   const pokemon = await api.getPokemon(searchPokemon);
   pokemon.displayInfo();
+});
+
+const btnUp = document.getElementById("btn-up");
+const btnDown = document.getElementById("btn-down");
+const scrollableElement = document.getElementById("pokemon-info");
+
+btnDown.addEventListener("click", () => {
+  scrollableElement.scrollTop += 10;
+  // Or, scroll smoothly to a specific position
+  // scrollableElement.scrollTo({
+  //   top: scrollableElement.scrollHeight,
+  //   behavior: "smooth",
+  // });
+});
+
+btnUp.addEventListener("click", () => {
+  scrollableElement.scrollTop -= 10;
 });
